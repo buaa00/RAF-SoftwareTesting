45504f5426718e65643edef1fdeb64e6f05441e2
Burga <sburgic15@raf.rs>
2019-01-12 03:58:17 +0100

    updateovan git ignore

--- App/app/src/main/java/model/errors/GpuLimitExceededError.java
+++ App/app/src/main/java/model/errors/GpuLimitExceededError.java
@@ -5,6 +5,8 @@ package model.errors;
  */
 public class GpuLimitExceededError extends MachineError {
 
+    private static final long serialVersionUID = 1L;
+
     public GpuLimitExceededError(String gpuCount, String gpuType) {
         super("GPU limit is exceeded! Current number of GPUs is "+gpuCount+"of type "+gpuType);
     }
